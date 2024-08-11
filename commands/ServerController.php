7c615a126899f5901bd518e135a366bc340b6645
dasauser <dasauser@gmail.com>
2024-08-11 23:45:44 +0300

    Изменен способ передачи токена

--- commands/ServerController.php
+++ commands/ServerController.php
@@ -30,40 +30,29 @@ class ServerController extends Controller implements MessageComponentInterface
 
     public function onOpen(ConnectionInterface $conn)
     {
-        /** @var \Psr\Http\Message\RequestInterface $httpRequest */
-        $httpRequest = $conn->httpRequest;
-
-        $params = [];
-        parse_str($httpRequest->getUri()->getQuery(), $params);
-        $token = $params['token'] ?? null;
-
-        if ($token !== 'validToken') {
-            $conn->send('Invalid token');
-            $conn->close();
-            return;
-        }
-
-        $userId = 1;
-
-        $connection = new Connection([
-            'scenario' => Connection::SCENARIO_OPEN,
-            'token' => $token,
-            'user_agent' => $httpRequest->getHeader('User-Agent'),
-            'user_id' => $userId,
-        ]);
-
-        if (!$connection->save()) {
-            $conn->send('can not open connection');
-            $conn->close();
-            throw new ServerErrorHttpException('can not open connection');
-        }
-
         $conn->send('connected');
-        $this->clients->attach($conn, $connection);
     }
 
     public function onMessage(ConnectionInterface $from, $msg)
     {
+        $decodedMsg = json_decode($msg, true);
+
+        if ($this->isAuthMsg($decodedMsg)) {
+            try {
+                $this->clients->attach($from, $this->auth($from, $decodedMsg));
+                $from->send('authenticated');
+            } catch (\Throwable $exception) {
+                $from->send($exception->getMessage());
+                $from->close();
+            }
+            return;
+        }
+
+        if (!$this->isAuthenticated($from)) {
+            $from->send('not authenticated');
+            return;
+        }
+
         foreach ($this->clients as $client) {
             if ($from === $client) {
                 continue;
@@ -74,7 +63,12 @@ class ServerController extends Controller implements MessageComponentInterface
 
     public function onError(ConnectionInterface $conn, \Exception $e)
     {
-        \Yii::getLogger()->log($e->getMessage(), LogLevel::ERROR);
+        $message = $e->getMessage();
+
+        \Yii::getLogger()->log($message, LogLevel::ERROR);
+
+        $conn->send('error: ' . $message);
+
         $conn->close();
     }
 
@@ -82,12 +76,59 @@ class ServerController extends Controller implements MessageComponentInterface
     {
         /** @var Connection $connection */
         $connection = $this->clients->offsetGet($conn);
+
         $connection->setScenario(COnnection::SCENARIO_CLOSE);
+
         if ($connection->update() === false) {
             $conn->send('can not close connection');
             throw new ServerErrorHttpException('can not close connection');
         }
+
         $this->clients->detach($conn);
+
         $conn->send('connection closed');
+    }
+
+    private function auth(ConnectionInterface $conn, array $msg): Connection
+    {
+        /** @var \Psr\Http\Message\RequestInterface $httpRequest */
+        $httpRequest = $conn->httpRequest;
+
+        $connection = new Connection([
+            'scenario' => Connection::SCENARIO_OPEN,
+            'token' => $msg['token'],
+            'user_agent' => $httpRequest->getHeader('User-Agent')[0] ?? null,
+            'user_id' => $this->getUserId($msg['token']),
+        ]);
+
+        if (!$connection->save()) {
+            throw new ServerErrorHttpException('can not open connection');
+        }
+
+        return $connection;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserId(string $token): mixed
+    {
+        // поиск юзера по токену.
+        return 1;
+    }
+
+    private function isAuthMsg(array $decodedMsg): bool
+    {
+        return count($decodedMsg) === 2
+            && isset($decodedMsg['type'])
+            && $decodedMsg['type'] === 'auth'
+            && isset($decodedMsg['token'])
+            && is_string($decodedMsg['token'])
+            ;
+    }
+
+    private function isAuthenticated(ConnectionInterface $from): bool
+    {
+        return $this->clients->offsetExists($from);
     }
 }
