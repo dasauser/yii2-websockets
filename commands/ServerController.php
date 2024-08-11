27e615c24032ad03b94a835c08f783c6eb9a32e6
dasauser <dasauser@gmail.com>
2024-08-11 20:31:15 +0300

    Добавлен вебсокет-сервер

---
+++ commands/ServerController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace app\commands;
+
+use daemons\Chat;
+use Ratchet\ConnectionInterface;
+use Ratchet\MessageComponentInterface;
+use Ratchet\Server\IoServer;
+use yii\console\Controller;
+
+class ServerController extends Controller implements MessageComponentInterface
+{
+    private \SplObjectStorage $clients;
+
+    public function __construct($id, $module, $config = [])
+    {
+        parent::__construct($id, $module, $config);
+        $this->clients = new \SplObjectStorage;
+    }
+
+    public function actionStart(int $port = 21080)
+    {
+        $server = IoServer::factory($this, $port);
+        $server->run();
+    }
+
+    public function onOpen(ConnectionInterface $conn)
+    {
+        $this->clients->attach($conn);
+        echo "New connection! ({$conn->resourceId})\n";    }
+
+    public function onClose(ConnectionInterface $conn)
+    {
+        // The connection is closed, remove it, as we can no longer send it messages
+        $this->clients->detach($conn);
+
+        echo "Connection {$conn->resourceId} has disconnected\n";
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e)
+    {
+        echo "An error has occurred: {$e->getMessage()}\n";
+
+        $conn->close();
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg)
+    {
+        $numRecv = count($this->clients) - 1;
+        echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
+            , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
+
+        foreach ($this->clients as $client) {
+            if ($from !== $client) {
+                // The sender is not the receiver, send to each client connected
+                $client->send($msg);
+            }
+        }
+    }
+}
