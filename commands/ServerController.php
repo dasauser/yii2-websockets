428291b7992bdafdfe47124ed08db38d77bb6746
dasauser <dasauser@gmail.com>
2024-08-11 23:19:29 +0300

    Рефакторинг

--- commands/ServerController.php
+++ commands/ServerController.php
@@ -62,6 +62,22 @@ class ServerController extends Controller implements MessageComponentInterface
         $this->clients->attach($conn, $connection);
     }
 
+    public function onMessage(ConnectionInterface $from, $msg)
+    {
+        foreach ($this->clients as $client) {
+            if ($from === $client) {
+                continue;
+            }
+            $client->send($msg);
+        }
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e)
+    {
+        \Yii::getLogger()->log($e->getMessage(), LogLevel::ERROR);
+        $conn->close();
+    }
+
     public function onClose(ConnectionInterface $conn)
     {
         /** @var Connection $connection */
@@ -73,24 +89,5 @@ class ServerController extends Controller implements MessageComponentInterface
         }
         $this->clients->detach($conn);
         $conn->send('connection closed');
-    }
-
-    public function onError(ConnectionInterface $conn, \Exception $e)
-    {
-        \Yii::getLogger()->log($e->getMessage(), LogLevel::ERROR);
-        $conn->close();
-    }
-
-    public function onMessage(ConnectionInterface $from, $msg)
-    {
-        $numRecv = count($this->clients) - 1;
-        echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
-            , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
-
-        foreach ($this->clients as $client) {
-            if ($from !== $client) {
-                $client->send($msg);
-            }
-        }
     }
 }
